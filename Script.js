7f5e601542d72313a3eb5e2de15477f5e5f774b6
Sergjalo <Sergjalo@gmail.com>
2016-06-24 13:25:04 +0300

    addVarReading
    predefine all elements with vars. they are names of objects separated by
    spaces.
    vListObjectsMain - last obj is pop-up list and it should be shifted more
    then other
    vListObjectsDet
    vListObjectsBeyondGrid
    vListMainAndDetailGrids - names of Main and after space Detail grids.

--- Script.js
+++ Script.js
@@ -8,11 +8,73 @@ Qva.AddDocumentExtension('centerAlignPENSKE', function() {
 	var elementsUnderGrid;	 // set of elements under Main grid
 	var elementsUnderGridDet; // set of elements under Detail grid
 	var elementsBeyondGrid;	 // one textbox
-	var grName = "CH31";	// name of the Main grid
-	var grNameDet = "CH60"; // name of the Detail grid
+
+	var	docQv = Qv.GetCurrentDocument();
+	docQv.GetAllVariables(function(vars) {
+		var sNames;
+		for (var i = 0; i < vars.length; i++) {
+			if (vars[i].name.indexOf("vListObjectsMain")>-1) 		
+			{   
+				sNames=vars[i].value.split(" ");
+				listPagesMain= $(".QvFrame.Document_"+sNames[sNames.length-1]); 
+				elementsUnderGrid = $(".QvFrame.Document_"+sNames[0]);
+				for (var j=1; j<sNames.length-1; j++){
+					elementsUnderGrid=elementsUnderGrid.add($(".QvFrame.Document_"+sNames[j]));
+				}
+			}
+			if (vars[i].name.indexOf("vListObjectsDet")>-1)
+			{   
+				sNames=vars[i].value.split(" ");
+				listPagesDet= $(".QvFrame.Document_"+sNames[sNames.length-1]); 
+				elementsUnderGridDet = $(".QvFrame.Document_"+sNames[0]);
+				for (var j=1; j<sNames.length-1; j++){
+					elementsUnderGridDet=elementsUnderGridDet.add($(".QvFrame.Document_"+sNames[j]));
+				}
+			}
+			if (vars[i].name.indexOf("vListObjectsBeyondGrid")>-1)	
+			{   
+				sNames=vars[i].value.split(" ");
+				elementsBeyondGrid = $(".QvFrame.Document_"+sNames[0]);
+				for (var j=1; j<sNames.length; j++){
+					elementsBeyondGrid=elementsBeyondGrid.add($(".QvFrame.Document_"+sNames[j]));
+				}
+			}
+			if (vars[i].name.indexOf("vListMainAndDetailGrids")>-1)	 
+			{
+				sNames=vars[i].value.split(" ");
+				grMain=$(".QvFrame.Document_"+sNames[0]);
+				grDet =$(".QvFrame.Document_"+sNames[1]);
+			}
+		}
+	});
+		
 	//------------------------------------------------------------------- EPAM
 	
 	function centerIt() {
+		
+		//------------------------------------------------------------------- EPAM
+		if (elementsUnderGrid!=undefined) 
+		{
+			
+			if (grMain.css("display")=="none" )
+			{
+				var grMainY = grDet.position().top+grDet.outerHeight();
+				elementsUnderGridDet.css("top", grMainY+10);
+				listPagesDet.css("top", grMainY+48);
+			}	
+			else {
+				var grMainY = grMain.position().top+grMain.outerHeight();
+				elementsUnderGrid.css("top", grMainY+10);
+				listPagesMain.css("top", grMainY+48);
+			}	
+			// change height of underlying textbox
+			elementsBeyondGrid.each(function(){
+				$(this).css("height", grMainY-$(this).position().top+60).children().filter(".QvContent").css("height", grMainY-$(this).position().top+60);
+			});
+		}
+		
+		//------------------------------------------------------------------- EPAM	
+
 		if(!($("body").hasClass("centerAlign"))) {
 			$("body").addClass("centerAlign");
 			//wrap a container around the whole document and center it.
@@ -35,43 +97,6 @@ Qva.AddDocumentExtension('centerAlignPENSKE', function() {
 		});
 		$(".centerAlign .master").css("width", maxRight + "px");
 		$(".qvtr-tabs").css("width", $(".master").width() + "px");
-		
-		//------------------------------------------------------------------- EPAM
-		
-		//------------------------------------------------------------------- objects under grid move
-		var grMain=$(".QvFrame").filter(":visible").filter(".Document_"+grName+", .Document_"+grNameDet);
-		var grMainY = grMain.position().top+grMain.outerHeight();
-		
-		// shift buttons up to grid 
-		if ((elementsUnderGrid==undefined) && (grMain.attr("class").indexOf("Document_"+grName)>=0)) {
-			elementsUnderGrid=$(".QvFrame").filter(function () {
-				return grMainY< $(this).position().top;
-			});
-		}	
-		if ((elementsUnderGridDet==undefined)  && (grMain.attr("class").indexOf("Document_"+grNameDet)>=0)) {
-			elementsUnderGridDet=$(".QvFrame").filter(function () {
-				return grMainY< $(this).position().top;
-			});
-		}	
-		if (elementsUnderGridDet!=undefined) {
-			elementsUnderGridDet.css("top", grMainY+10);
-		}	
-		if (elementsUnderGrid!=undefined) {
-			elementsUnderGrid.css("top", grMainY+10);
-		}	
-		if (elementsBeyondGrid==undefined) {
-			elementsBeyondGrid=$(".QvFrame").filter(function () {
-				return (grMainY< $(this).position().top+$(this).outerHeight()) && (grMainY> $(this).position().top);
-			});
-		}	
-		
-		elementsBeyondGrid.each(function(){
-			$(this).css("height", grMainY-$(this).position().top+60).children().filter(".QvContent").css("height", grMainY-$(this).position().top+60);
-			console.log("grMainY="+grMainY+" topNew= "+ $(this).position().top + " hg="+$(this).outerHeight()+" css = "+$(this).css("height"));
-			console.log($(this).attr("class"));
-		})
-		//------------------------------------------------------------------- EPAM	
-		
 	}
 	_this.Document.SetOnUpdateComplete(centerIt);
 });
